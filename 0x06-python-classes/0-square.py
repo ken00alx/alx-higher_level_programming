7da3f45655e8e5a25dcdc2e179a6bd724d54d4e9
ken00alx <kenny933@gmail.com>
2023-03-29 09:16:03 -0700

    size of square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Square module """
-
+""" Creating an empty square"""
 
 class Square:
     """ Declares a square class """
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Square module """
+""" Square module size """
 
 
 class Square:
