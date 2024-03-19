804073893ebfed7012f94b7f02e3594895190502
ken00alx <kennye933@gmail.com>
2024-03-19 09:03:22 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return [num % 2 == 0 for num in my_list]
-
-original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = divisible_by_2(original_list)
-print(result)
-
+    boolist = my_list[:]
+    for count, i in enumerate(my_list):
+        if i % 2 == 0:
+            boolist[count] = True
+        else:
+            boolist[count] = False
+    return(boolist)
