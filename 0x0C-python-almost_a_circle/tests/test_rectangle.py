345bc1ffc87218d7b8408f2d37e8140d9b7979b5
ken00alx <kennye933@gmail.com>
2024-04-24 21:44:19 -0700

    Test_files

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""Unit test class Rectangle"""
+"""Module to test the function rectangle"""
 import unittest
-import json
-import sys
-import os
-from models.rectangle import Rectangle
+import rectangle
+
+
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_output(self):
+        """Function to test the output the function rectangle"""
+
+        self.assertAlmostEqual(rectangle(,)
+
+    def test_rectangle_input(self):
+        """Function to test the input the function rectangle"""
+
+        self.assertRaises(TypeError, rectangle, )
